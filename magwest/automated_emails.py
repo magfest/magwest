f6681dc2b28a24ad6acc57a6b0fa1eb220643ff4
Rob Ruana <rob@robruana.com>
2017-03-13 18:45:09 -0700

    Fixes #1: Adds static ident arg to each AutomatedEmail instance

--- magwest/automated_emails.py
+++ magwest/automated_emails.py
@@ -1,17 +1,22 @@
 from magwest import *
 
-# With "MAGFest Laboratories" as a new event, we find that the phrasing "Staffing again"
-# doesn't differentiate this event, so we remove it.
-AutomatedEmail.instances['Want to staff {}?'.format(c.EVENT_NAME)] = AutomatedEmail.instances.pop('Want to staff {} again?'.format(c.EVENT_NAME))
+# With "MAGWest" as a new event, we find that the phrasing "Want to staff again?"
+# doesn't differentiate this event, so we change it to just plain "Want to staff?"
+if 'volunteer_again_inquiry' in AutomatedEmail.instances:
+    AutomatedEmail.instances['volunteer_again_inquiry'].subject = 'Want to staff {}?'.format(c.EVENT_NAME)
 
 AutomatedEmail(Attendee, '{EVENT_NAME} food for guests', 'guest_food_restrictions.txt',
            lambda a: a.badge_type == c.GUEST_BADGE,
+           ident='magwest_guest_food_restrictions',
            sender="MAGFest Staff Suite <chefs@magfest.org>")
 AutomatedEmail(Attendee, '{EVENT_NAME} hospitality suite information', 'guest_food_info.txt',
            lambda a: a.badge_type == c.GUEST_BADGE,
+           ident='magwest_guest_food_info',
            sender="MAGFest Staff Suite <chefs@magfest.org>")
 AutomatedEmail(Attendee, '{EVENT_NAME} Volunteer Food', 'volunteer_food_info.txt',
            lambda a: a.staffing and days_before(7, c.FINAL_EMAIL_DEADLINE),
+           ident='magwest_volunteer_food_info',
            sender="MAGFest Staff Suite <chefs@magfest.org>")
 AutomatedEmail(Attendee, '{EVENT_NAME} FAQ', 'prefest_faq.html',
-               lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE))
+               lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
+               ident='magwest_prefest_faq')
