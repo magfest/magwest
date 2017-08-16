5bd871e3d3a2d68a49bcc32773aeb07a19d6889e
Rob Ruana <rob@robruana.com>
2017-08-16 12:41:30 -0400

    Adds PC Gaming Survey AutomatedEmail() for attendees who are interested in LAN

--- magwest/automated_emails.py
+++ magwest/automated_emails.py
@@ -9,14 +9,23 @@ AutomatedEmail(Attendee, '{EVENT_NAME} food for guests', 'guest_food_restriction
            lambda a: a.badge_type == c.GUEST_BADGE,
            ident='magwest_guest_food_restrictions',
            sender="MAGFest Staff Suite <chefs@magfest.org>")
+
 AutomatedEmail(Attendee, '{EVENT_NAME} hospitality suite information', 'guest_food_info.txt',
            lambda a: a.badge_type == c.GUEST_BADGE,
            ident='magwest_guest_food_info',
            sender="MAGFest Staff Suite <chefs@magfest.org>")
+
 AutomatedEmail(Attendee, '{EVENT_NAME} Volunteer Food', 'volunteer_food_info.txt',
            lambda a: a.staffing and days_before(7, c.FINAL_EMAIL_DEADLINE),
            ident='magwest_volunteer_food_info',
            sender="MAGFest Staff Suite <chefs@magfest.org>")
+
 AutomatedEmail(Attendee, '{EVENT_NAME} FAQ', 'prefest_faq.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='magwest_prefest_faq')
+
+AutomatedEmail(Attendee, '{EVENT_NAME} PC Gaming Survey', 'pc_gaming_survey.html',
+           lambda a: c.LAN in a.interests_ints,
+           ident='pc_gaming_survey',
+           needs_approval=True,
+           sender="MAGWest LAN Staff <lan@magwest.org>")
