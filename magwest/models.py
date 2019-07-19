b137a2b2a97f4eda06e83c346a61c64ca7388873
Victoria Earl <kitsuta@gmail.com>
2019-07-19 14:14:19 -0400

    Simplify overrides Thanks to a change in the main plugin's PR, we don't even need to override num_free_event_shirts, so we'll remove it now.

--- magwest/models.py
+++ magwest/models.py
@@ -6,8 +6,3 @@ class Attendee:
     def gets_staff_shirt(self):
         # This is technically just a generic MAGWest shirt, so volunteers are also eligible
         return self.staffing
-
-    @property
-    def num_free_event_shirts(self):
-        # Volunteers also get to select if they want an event shirt or not, so they do not get a free shirt by default
-        return self.num_event_shirts
