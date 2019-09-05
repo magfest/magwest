a21abf3b4fd54a4c00c6adc209a95a01c786e991
Victoria Earl <kitsuta@gmail.com>
2019-09-05 15:45:03 -0400

    Fix scheduled email syntax Now it works!

--- magwest/automated_emails.py
+++ magwest/automated_emails.py
@@ -53,12 +53,11 @@ StopsEmailFixture(
     when=days_before(1, c.FINAL_EMAIL_DEADLINE),
     ident='volunteer_shift_schedule_correction')
 
-
 AutomatedEmailFixture(
-    Attendee, 'Last Chance for MAGWest ' + c.EVENT_YEAR + ' bonus swag!', 'attendee_swag_promo.html',
-    lambda a: (
-        a.can_spam
-        and (a.paid == c.HAS_PAID or a.paid == c.NEED_NOT_PAY or (a.group and a.group.amount_paid)),
+    Attendee,
+    'Last Chance for MAGWest {EVENT_YEAR} bonus swag!',
+    'attendee_swag_promo.html',
+    lambda a: a.can_spam and a.badge_status == c.COMPLETED_STATUS and a.amount_extra < c.SEASON_LEVEL,
     when=days_before(2, c.EPOCH),
     sender='MAGWest Merch Team <merch@magwest.org>',
     ident='magwest_bonus_swag_reminder_last_chance')
