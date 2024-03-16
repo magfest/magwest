f63ca6b08e988d02aa682e24aa1206a12b4cf255
Victoria Earl <kitsuta@gmail.com>
2024-03-15 22:22:55 -0400

    Update dealer and panel emails for 2024 Changes requested via Slack!

--- magwest/automated_emails.py
+++ magwest/automated_emails.py
@@ -1,9 +1,26 @@
 from datetime import timedelta
 
-from uber.automated_emails import AutomatedEmailFixture, BandEmailFixture, StopsEmailFixture
+from uber.automated_emails import AutomatedEmailFixture, MarketplaceEmailFixture, StopsEmailFixture
 from uber.config import c
-from uber.models import Attendee
+from uber.models import Attendee, AutomatedEmail
 from uber.utils import before, days_before, days_after
+
+
+MarketplaceEmailFixture(
+        f'Waitlisted - {c.EVENT_NAME_AND_YEAR}',
+        'dealers/waitlisted.txt',
+        lambda g: g.status == c.WAITLISTED,
+        # query=Group.status == c.WAITLISTED,
+        needs_approval=True,
+        ident='dealer_reg_waitlisted')
+
+MarketplaceEmailFixture(
+        f'About your Declined Dealer\'s Application - {c.EVENT_NAME_AND_YEAR}',
+        'dealers/declined.txt',
+        lambda g: g.status == c.DECLINED,
+        # query=Group.status == c.DECLINED,
+        needs_approval=True,
+        ident='dealer_reg_declined')
 
 
 AutomatedEmailFixture(
@@ -14,6 +31,7 @@ AutomatedEmailFixture(
     ident='magwest_guest_food_restrictions',
     sender="MAGWest Tea Room <tearoom@magwest.org>")
 
+
 AutomatedEmailFixture(
     Attendee,
     '{EVENT_NAME} hospitality suite information',
@@ -21,6 +39,7 @@ AutomatedEmailFixture(
     lambda a: a.badge_type == c.GUEST_BADGE,
     ident='magwest_guest_food_info',
     sender="MAGWest Tea Room <tearoom@magwest.org>")
+
 
 AutomatedEmailFixture(
     Attendee,
@@ -30,6 +49,7 @@ AutomatedEmailFixture(
     ident='magwest_volunteer_food_info',
     sender="MAGWest Tea Room <tearoom@magwest.org>")
 
+
 AutomatedEmailFixture(
     Attendee,
     '{EVENT_NAME} FAQ',
@@ -37,14 +57,15 @@ AutomatedEmailFixture(
     lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
     ident='magwest_prefest_faq')
 
+
 AutomatedEmailFixture(
     Attendee,
     '{EVENT_NAME} PC Gaming Survey',
     'pc_gaming_survey.html',
     lambda a: c.LAN in a.interests_ints,
     ident='pc_gaming_survey',
-    needs_approval=True,
     sender="MAGWest LAN Staff <lan@magwest.org>")
+
 
 StopsEmailFixture(
     'CORRECTION to the {EVENT_NAME} ({EVENT_DATE}) Shift Schedule Email',
@@ -52,6 +73,7 @@ StopsEmailFixture(
     lambda a: c.SHIFTS_CREATED and a.weighted_hours,
     when=days_before(1, c.FINAL_EMAIL_DEADLINE),
     ident='volunteer_shift_schedule_correction')
+
 
 AutomatedEmailFixture(
     Attendee,
@@ -62,3 +84,12 @@ AutomatedEmailFixture(
     when=before(c.EPOCH - timedelta(days=2)),
     sender='MAGWest Merch Team <merch@magwest.org>',
     ident='magwest_bonus_swag_reminder_last_chance')
+
+
+AutomatedEmail.email_overrides.extend([
+    ('panel_accepted', 'subject', f"Approved - {c.EVENT_NAME_AND_YEAR} Panel Application"),
+    ('panel_declined', 'subject', f"Declined - {c.EVENT_NAME_AND_YEAR} Panel Application"),
+    ('panel_waitlisted', 'subject', f"Waitlisted - {c.EVENT_NAME_AND_YEAR} Panel Application"),
+    ('panel_accept_reminder', 'subject', f"Last Chance to Confirm Your Panel - {c.EVENT_NAME_AND_YEAR}"),
+    ('panel_scheduled', 'subject', f"Your Panel has been Scheduled - {c.EVENT_NAME_AND_YEAR}")
+    ])
--- magwest/models.py
+++ magwest/models.py
@@ -2,11 +2,17 @@ from uber.models import Session
 from uber.models.types import DefaultColumn as Column, MultiChoice
 from uber.config import c
 
+
 @Session.model_mixin
 class Attendee:
     @property
     def num_free_event_shirts(self):
         return 1 if self.badge_type == c.STAFF_BADGE else self.volunteer_event_shirt_eligible
+
+    @property
+    def approved_panel_apps(self):
+        return [panel.name for panel in self.panel_applications if panel.status == c.ACCEPTED]
+
 
 @Session.model_mixin
 class GuestMerch:
