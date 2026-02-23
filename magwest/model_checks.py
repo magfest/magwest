5a87fe3945633c1c8a49241df0c3d2a2e832255d
Victoria Earl <kitsuta@gmail.com>
2026-02-23 22:01:03 +0100

    Update to new form system Also removes the extra model check for under-13 badges since it's been moved to the main plugin.

--- magwest/model_checks.py
+++ magwest/model_checks.py
@@ -12,17 +12,3 @@ from uber.models import Choice, DefaultColumn as Column, Session
 from uber.jinja import template_overrides
 from uber.utils import localized_now, valid_email, get_age_from_birthday
 
-
-@prereg_validation.Attendee
-def attendee_badge_under_13(attendee):
-    if c.AT_THE_CON:
-        return
-    
-    if not attendee.is_new and attendee.badge_status != c.PENDING_STATUS \
-            or attendee.unassigned_group_reg or attendee.placeholder:
-        return
-
-    if c.CHILD_BADGE in c.PREREG_BADGE_TYPES and attendee.birthdate and attendee.badge_type == c.ATTENDEE_BADGE and (
-            get_age_from_birthday(attendee.birthdate, c.NOW_OR_AT_CON) < 13):
-        return ('badge_type', "If you will be 12 or younger at the start of {}, "
-                "please select the 12 and Under badge instead of an Attendee badge.".format(c.EVENT_NAME))
