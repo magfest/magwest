03f4b7557f0bf093c97284cefaa2171e391a6b75
Victoria Earl <kitsuta@gmail.com>
2026-04-27 22:58:09 +0200

    Hide no signups option for West

--- magwest/forms.py
+++ magwest/forms.py
@@ -7,3 +7,24 @@ from uber.forms import MagForm
 @MagForm.form_mixin
 class AdminTableInfo:
     night_market = BooleanField(f"This {c.DEALER_TERM} is part of the Night Market.")
+
+
+@MagForm.form_mixin
+class BaseJobInfo:
+    no_slots = HiddenField('')
+
+
+@MagForm.form_mixin
+class JobInfo:
+    slots = IntegerField('Slots', default=1)
+
+
+@MagForm.form_mixin
+class JobTemplateInfo:
+    min_slots = IntegerField('Minimum # Slots', default=1)
+
+    def no_slots_label(self):
+        return ''
+    
+    def no_slots_desc(self):
+        return ""
